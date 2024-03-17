83268af5948b5d95f0e80dc8a073dd02f3bb4f74
yen <oe327188@gmail.com>
2024-03-17 15:21:04 +0800

    move file to package

--- mqtt_factory.py
+++
@@ -1,9 +0,0 @@
-from .i_client import IMqttClient
-from .client import GMqttClient
-
-class MqttFactory:
-    def __init__(self) -> None:
-        pass
-
-    def createGMqttClient(self) -> IMqttClient:
-        return GMqttClient()
